f8baf671a79a6787922f90404902c17729472dff
Felix Krull <f_krull@gmx.de>
2018-03-31 22:39:26 +0200

    Add convenience scripts for building and deploying

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -eu
+SYNCDIR=/home/vagrant/sync
+TAG=atomic-debian
+OSTREE=/home/vagrant/ostree
+CONF=$SYNCDIR/strap.conf
+REF=debian/9/x86_64/minimal
+
+docker build -t $TAG $SYNCDIR
+docker run \
+    --rm \
+    -v $OSTREE:/ostree:z \
+    -v $CONF:/conf:ro,z \
+    $TAG \
+    /conf \
+    $REF
---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eu
+OSTREE=/home/vagrant/ostree
+REF=debian/9/x86_64/minimal
+OS=debian
+
+ostree pull-local $OSTREE $REF
+ostree admin deploy --os $OS $REF
