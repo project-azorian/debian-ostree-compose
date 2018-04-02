09292824d862d1d3924226b18954b66af04b4c8e
Felix Krull <f_krull@gmx.de>
2018-04-03 00:57:39 +0200

    Various changes to the build script

--- build-atomic-debian.sh
+++ build-atomic-debian.sh
@@ -10,12 +10,47 @@ rm -rf $WORKDIR
 mkdir -p $WORKDIR
 cd $WORKDIR
 
+# ----
+
 dpkg-deb -x /debs/ostree-boot_2018.2-1~bpo9+1_amd64.deb .
+
+# ----
+
+mkdir dev
+mount -o bind /dev dev
+
 multistrap -d $WORKDIR -f $STRAPCONF
+
+umount dev
+
+# ----
+
+#mkdir -p etc/ssh
+#touch etc/ssh/ssh_host_rsa_key
+#touch etc/ssh/ssh_host_ecdsa_key
+#touch etc/ssh/ssh_host_ed25519_key
+
+
+# ----
+
+#mount -o bind /dev dev
+
+#cat > setup.sh <<EOF
+#export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
+#export LC_ALL=C LANGUAGE=C LANG=C
+#/var/lib/dpkg/info/dash.preinst install
+#dpkg --configure -a
+#EOF
+#chroot . /bin/bash /setup.sh
+#rm setup.sh
+
+#umount dev
 
 # ----
 
 rm -rf dev/*
+rm -rf var/*
+#rm etc/ssh/ssh_host_*_key
 
 # ----
 
@@ -25,21 +60,22 @@ ln -s sysroot/ostree ostree
 
 for d in bin lib lib64 sbin; do
     mkdir -p usr/$d
-    cp -a $d/* usr/$d
-    rm -r $d
+    cp -Pr $d/* usr/$d
+    rm -rf $d
     ln -s usr/$d $d
 done
 
-rm -r var/*
-rm -r home opt srv root usr/local mnt media tmp
+rm -r home opt srv root usr/local mnt media
 ln -s var/home home
 ln -s var/opt opt
 ln -s var/srv srv
 ln -s var/roothome root
 ln -s var/local usr/local
 ln -s var/mnt mnt
-ln -s sysroot/tmp tmp
+#ln -s sysroot/tmp tmp
 ln -s run/media media
+
+# ----
 
 mkdir -p --mode=0755 usr/etc/tmpfiles.d
 cat > usr/etc/tmpfiles.d/debian-atomic.conf <<EOF
@@ -62,9 +98,17 @@ d /var/mnt 0755 root root -
 d /run/media 0755 root root -
 EOF
 
+# ----
+
+ln -s usr/lib/systemd/system/ostree-remount.service usr/lib/systemd/system/local-fs.target.wants/
+
+# ----
+
 KVER=$(ls usr/lib/modules)
 cp boot/vmlinuz-$KVER usr/lib/modules/$KVER/vmlinuz
 cp boot/initrd.img-$KVER usr/lib/modules/$KVER/initramfs.img
+cp boot/System.map-$KVER usr/lib/modules/$KVER/System.map
+cp boot/config-$KVER usr/lib/modules/$KVER/config
 rm -r boot/* initrd.img initrd.img.old vmlinuz vmlinuz.old
 
 # ----
